1ff8c63f2024c14f61e89ad83e370b39a40410ee
KimThoa2504 <tkthoa0425@gmail.com>
2026-05-17 14:30:15 +0700

    Update Program.cs

--- UniversityResearchManagement/Program.cs
+++ UniversityResearchManagement/Program.cs
@@ -27,6 +27,19 @@ namespace UniversityResearchManagement
                     new System.Text.Json.Serialization.JsonStringEnumConverter()
                     );
             });
+
+            builder.Services.AddCors(options =>
+            {
+                options.AddPolicy("AllowAngular",
+                    policy =>
+                    {
+                        policy
+                            .WithOrigins("http://localhost:4200")
+                            .AllowAnyHeader()
+                            .AllowAnyMethod();
+                    });
+            });
+
             // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
             builder.Services.AddOpenApi();
 
@@ -35,6 +48,8 @@ namespace UniversityResearchManagement
 
             var app = builder.Build();
 
+            app.UseCors("AllowAngular");
+            
             // Configure the HTTP request pipeline.
             if (app.Environment.IsDevelopment())
             {
